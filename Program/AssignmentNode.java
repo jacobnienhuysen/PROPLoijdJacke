a8704d41e920c1a3680394d33b442d028dfab10c
jacobnienhuysen <jacob.nienhuysen@gmail.com>
2015-11-13 16:36:12 +0100

    Byggt ut tokenizern
    Lagt till grammatiken i tokenizern.

---
+++ Program/AssignmentNode.java
@@ -0,0 +1,12 @@
+
+public class AssignmentNode implements INode{
+	
+	public Object evaluate(Object[] args) throws Exception{
+		return
+	}
+	
+	public void buildString(StringBuilder builder, int tabs){
+		//assign = id , ’=’ , expr , ’;’ ;
+	}
+
+}
--- Program/Tokenizer.java
+++ Program/Tokenizer.java
@@ -8,7 +8,6 @@ public class Tokenizer implements ITokenizer {
 	IScanner scan;
 	
 	public Tokenizer(){
-		
 		scan = new Scanner();
 		
 	}
@@ -19,10 +18,53 @@ public class Tokenizer implements ITokenizer {
 	public void open(String fileName) throws IOException, TokenizerException{
 		scan.open(fileName);
 	}
+	
 	/**
 	 * Returns the current token in the stream.
 	 */
 	public Lexeme current(){
+		
+		Lexeme temp = new Lexeme(null,null);
+		
+		return temp;
+	}
+	
+	
+	public void assignment(){
+		try{
+			char temp = scan.current();
+			//assign = id , ’=’ , expr , ’;’ ;
+			if(Character.isLowerCase(temp)){
+
+				while(scan.current() != '='){
+					moveNext();
+				}
+				
+				expression();
+			}
+			
+		}catch(IOException e){
+			
+		}catch(TokenizerException e){
+			
+		}
+	}
+	
+	public void expression(){
+		//expr = term , [ ( ’+’ | ’-’ ) , expr ] ;
+		
+		
+	}
+	
+	public void term(){
+		//term = factor , [ ( ’*’ | ’/’) , term] ;
+		
+		
+	}
+	
+	public void factor(){
+		//factor = int | ’(’ , expr , ’)’ ;
+		
 		
 	}
 
@@ -30,13 +72,13 @@ public class Tokenizer implements ITokenizer {
 	 * Moves current to the next token in the stream.
 	 */
 	public void moveNext() throws IOException, TokenizerException{
-		
+		scan.moveNext();
 	}
 
 	/**
 	 * Closes the file and releases any system resources associated with it.
 	 */
 	public void close() throws IOException{
-		
+		scan.close();
 	}
 }
